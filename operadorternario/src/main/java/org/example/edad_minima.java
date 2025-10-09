83f90f5748474672442c37ee8198c90c0a304194
AlexPerse10 <spuknitfire@gmail.com>
2025-10-09 23:54:42 +0200

    ejercicio ternaria mayor de edad

--- operadorternario/src/main/java/org/example/edad_minima.java
+++ operadorternario/src/main/java/org/example/edad_minima.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class edad_minima {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("introduce tu edad");
+        int edad = sc.nextInt();
+        String mayormenor;
 
+        mayormenor = edad>=18 ? "eres mayor de edad" : "eres menor de edad";
+
+        System.out.println(mayormenor);
     }
 }
