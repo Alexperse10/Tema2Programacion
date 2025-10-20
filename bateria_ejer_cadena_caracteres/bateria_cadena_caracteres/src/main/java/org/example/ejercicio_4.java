869b969078f517b8effd464b212636ceb9c1d4cf
AlexPerse10 <spuknitfire@gmail.com>
2025-10-20 14:15:09 +0200

    se comienza con el ejercicio 5

--- bateria_ejer_cadena_caracteres/bateria_cadena_caracteres/src/main/java/org/example/ejercicio_4.java
+++ bateria_ejer_cadena_caracteres/bateria_cadena_caracteres/src/main/java/org/example/ejercicio_4.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class ejercicio_4 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("introduce una frase");
+        String frase = sc.nextLine();
+        System.out.println("introduce una palabra");
+        String palabra = sc.nextLine();
 
+        
     }
 }
