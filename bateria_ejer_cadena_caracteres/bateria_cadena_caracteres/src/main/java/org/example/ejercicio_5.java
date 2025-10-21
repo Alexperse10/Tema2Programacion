bea4da1ee00e485629a811c6d7384de3f638a884
AlexPerse10 <spuknitfire@gmail.com>
2025-10-21 12:53:55 +0200

    se realiza ejercicio 5, falta por terminar

--- bateria_ejer_cadena_caracteres/bateria_cadena_caracteres/src/main/java/org/example/ejercicio_5.java
+++ bateria_ejer_cadena_caracteres/bateria_cadena_caracteres/src/main/java/org/example/ejercicio_5.java
@@ -5,11 +5,28 @@ import java.util.Scanner;
 public class ejercicio_5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("introduce una frase");
-        String frase = sc.nextLine();
-        System.out.println("introduce una palabra");
-        String palabra = sc.nextLine();
-
+        // sintaxis es boolean resultado = texto.contains("subcadena")
+        int contador=1;
+        final int totaloportunidades=4;
+        boolean acertado = false;
+           System.out.println("introduce una frase");
+           String frase = sc.nextLine();
+        do {
+           System.out.println("introduce una palabra");
+           String palabra = sc.nextLine();
+           boolean contiene = frase.contains(palabra);
+           if (contiene){
+               System.out.println("enhorabuena has acertado");
+               acertado =true;
+           }else {
+               int oportunidades = totaloportunidades- contador;
+               System.out.println("lo siento no has acertado, te quedan "+oportunidades);
+               if (oportunidades ==0){
+                   System.out.println("lo siento no te queda ninguna oportunidad");
+               }
+               contador ++;
+           }
+       }while (!acertado && contador<=totaloportunidades);
 
     }
 }
