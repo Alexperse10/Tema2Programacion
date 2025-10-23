e6e0c1eca240c9c59dc3006c8694ab8598a2bdd4
AlexPerse10 <spuknitfire@gmail.com>
2025-10-23 14:19:40 +0200

    ejercicios aleatorios sobre el tema 2

---
+++ ejercicios_aleatorios/untitled/src/main/java/org/example/contador_palabras.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class contador_palabras {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("introduce una frase");
+        String frase = sc.nextLine();
+
+        int numero_frase = frase.length();
+        String frase_sinespacios = frase.replace(" ","");
+        
+    }
+}
