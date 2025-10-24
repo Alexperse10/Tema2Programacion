eb8b85426453a442888ec7dbeda0bbd3a1720950
AlexPerse10 <spuknitfire@gmail.com>
2025-10-24 13:56:12 +0200

    ejercicio 4 hecho

--- bateria_problemas_pre-examen/bateria_ejercicios_pre-examen/src/main/java/org/example/ejercicio_4.java
+++ bateria_problemas_pre-examen/bateria_ejercicios_pre-examen/src/main/java/org/example/ejercicio_4.java
@@ -5,6 +5,41 @@ import java.util.Scanner;
 public class ejercicio_4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        boolean repetir = false;
+        do {
+
+
+            System.out.println("Introduce la nota del alumno");
+            int nota = sc.nextInt();
+
+            switch (nota) {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                    System.out.println("insuficiente");
+                    break;
+                case 5:
+                    System.out.println("suficiente");
+                    break;
+                case 6:
+                    System.out.println("bien");
+                    break;
+                case 7:
+                case 8:
+                    System.out.println("notable");
+                    break;
+                case 9:
+                case 10:
+                    System.out.println("sobresaliente");
+                    break;
+
+                default:
+                    System.out.println("ERROR valor incorrecto ");
+                    repetir = true;
+            }
+
+        }while (repetir);
     }
 }
