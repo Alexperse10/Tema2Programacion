fc34b7ae9d9fcbd8f1afc627e021deb2086fc589
AlexPerse10 <spuknitfire@gmail.com>
2025-10-27 14:19:31 +0100

    ejercicio 7 sin terminar

--- bateria_problemas_pre-examen/bateria_ejercicios_pre-examen/src/main/java/org/example/ejercicio_7.java
+++ bateria_problemas_pre-examen/bateria_ejercicios_pre-examen/src/main/java/org/example/ejercicio_7.java
@@ -7,5 +7,17 @@ public class ejercicio_7 {
         Scanner sc = new Scanner(System.in);
         System.out.println("introduce un numero");
         int numero = sc.nextInt();
+        String comienzo = "0,1";
+        String sin_comas = comienzo.replace(",","");
+        char primernumero = sin_comas.charAt(0);
+        char segundonumero = sin_comas.charAt(1);
+        int primernumero_int = Integer.parseInt(String.valueOf(0));
+        int segundonumero_int = Integer.parseInt(String.valueOf(1));
+        int sumaprimerasecuencia = primernumero_int + segundonumero_int; 
+        for (int i=0; i<numero; i++){
+
+
+        }
+
     }
 }
